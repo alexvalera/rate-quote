e7b3af8e4468b613a50c9bf9ccde12eeddc52efe
Alex Valera <alexv@tahzoo.com>
2020-10-29 01:26:18 -0400

    compose middleware

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,12 @@
-import { createStore, applyMiddleware } from "redux";
+import { createStore, applyMiddleware, compose } from "redux";
 import thunk from 'redux-thunk';
 import rootReducer from "../reducers/index";
 
+const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
 const store = createStore(
     rootReducer, 
-    applyMiddleware(thunk),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    composeEnhancer(applyMiddleware(thunk))
   );
 
 export default store;
